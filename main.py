d2e2a7638f8ac8666b4ae361fb4b9a51f6b84e5e
Hitriihoma <hitriihoma@gmail.com>
2025-05-12 21:00:45 +0500

    Add example with headers and keys

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ def main(doc, insert_image):
     return table
     
 if __name__ == "__main__":
+    '''
+    Examples 
+    hw_1: 
+        doc = Table(skiprows=0, num_col=2, key_col=None, h1=[None,None]
+        insert_image = './tests/examples/hw_1.jpg'
+    structure_1:
+        doc = Table(skiprows=2, num_col=3, key_col=1, h1=[1,2], h2=[2,2])
+        insert_image = './tests/examples/structure_1.jpg'
+    '''
     doc = Table(skiprows=0, num_col=2, key_col=None, h1=[None,None], h2=[None,None])
     insert_image = input('Insert path to image: ') # './tests/examples/hw_1.jpg'
     ocr_table = main(doc, insert_image)
