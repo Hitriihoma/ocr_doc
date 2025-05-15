2222cd93b61abed1ba3a6ed7cac85aed90589b39
Hitriihoma <hitriihoma@gmail.com>
2025-05-16 00:16:34 +0500

    Change app to FastAPI server

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ Created on Sat May  3 15:08:35 2025
 """
 
 from ocr.ocr_module import OCR_doc, Table
+from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+import uvicorn
 
-def main(doc, insert_image):
+app = FastAPI()
+
+class OcrInput(BaseModel):
+    img_path: str
+    structure: dict
+
+@app.post("/ocr")
+async def ocr_doc(insert_json: OcrInput):
+    insert_image = insert_json.img_path
+    doc = Table(skiprows=insert_json.structure['skiprows']
+                , num_col=insert_json.structure['num_col']
+                , key_col=insert_json.structure['key_col']
+                , h1=insert_json.structure['h1']
+                , h2=insert_json.structure['h2'])
     doc.load_image(insert_image)
-    HOCR = OCR_doc()
-    table = HOCR.ocr_table(doc)
+    ocr = OCR_doc()
+    table = ocr.ocr_table(doc)
     
-    return table
-    
+    return jsonable_encoder(table)
+
 if __name__ == "__main__":
-    '''
-    Examples 
-    hw_1: 
-        doc = Table(skiprows=0, num_col=2, key_col=None, h1=[None,None]
-        insert_image = './tests/examples/hw_1.jpg'
-    structure_1:
-        doc = Table(skiprows=2, num_col=3, key_col=1, h1=[2,1], h2=[2,2])
-        insert_image = './tests/examples/structure_1.jpg'
-    '''
-    doc = Table(skiprows=0, num_col=2, key_col=None, h1=[None,None], h2=[None,None])
-    insert_image = input('Insert path to image: ')
-    ocr_table = main(doc, insert_image)
-    print(ocr_table)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
