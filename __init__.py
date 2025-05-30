a7d82e8fd141b635932a3235ee708b108a76afff
Hitriihoma <hitriihoma@gmail.com>
2025-05-30 18:14:21 +0500

    Change version number

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Hitriihoma'
 __copyright__ = "2025, {}".format(__author__)
-__version__ = '1.0'
+__version__ = '0.1.1'
 __maintainer__ = ['Hitriihoma']
 __email__ = 'hitriihoma@gmail.com'
 __status__ = "Prototype"
